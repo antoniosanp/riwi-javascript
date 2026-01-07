198aa67ae0bf03ca0f82dd8dd01edbee8fcc1de3
angela MOnsalve <angelapmonsalve@gmail.com>
2026-01-07 05:40:29 -0500

    session day 2

--- semana-1/dia-1/02-tipos-datos.js
+++ semana-1/dia-1/02-tipos-datos.js
@@ -2,3 +2,11 @@
 Declara variables de distintos tipos.
 Muestra el tipo de cada una usando typeof.
 */
+
+let telephone = 3143456789
+let saludo = "hello everyone"
+
+console.log (typeof (telephone))
+
+
+
