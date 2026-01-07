25ba4c650e2b26e84649d39d0c889138a8777234
antonio <antonio.pulgarin97@gmail.com>
2026-01-07 08:40:09 -0500

    listo script contador

---
+++ semana-1/stitch_javascript_practice_dashboard/scriptContador.js
@@ -0,0 +1,33 @@
+
+// Contador
+
+// elementos dom
+
+const btnAddContador = document.getElementById("btnAddContador");
+const btnResetContador = document.getElementById("btnResetContador")
+const btnRemoveContador = document.getElementById("btnRemoveContador")
+const contadorTextSpan = document.getElementById("contadorTextSpan");
+
+
+//----funciones-------------------------
+function add(){
+    contadorTextSpan.textContent++
+
+}
+
+function remove(){
+
+    contadorTextSpan.textContent--
+}
+
+function reset(){
+    contadorTextSpan.textContent = 0
+
+}
+
+//---listeners-------------------------
+
+btnAddContador.addEventListener("click",add);
+btnRemoveContador.addEventListener("click",remove)
+btnResetContador.addEventListener("click",reset)
+//------------------------------------------
